fccc3e1ae5f4f401a7b22d9235624c71facc1951
VolodyaGG <vovalaptev468@gmail.com>
2025-06-01 18:12:41 +0300

    передача от дочерней до главной новый код

--- ReadingRFID/master_arduino.ino
+++ ReadingRFID/master_arduino.ino
@@ -1,33 +1,59 @@
-#include <Wire.h>
+#include <SPI.h>
+#include <MFRC522.h>
+#include <SoftwareSerial.h>
 
-byte slaveUID[4];
+#define RST_PIN 9
+#define SS_PIN 10
+
+#define RX_PIN 2
+#define TX_PIN 3
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+SoftwareSerial mySerial(RX_PIN, TX_PIN);
+
+int childCounter = 0;
+
+unsigned long lastScanTime = 0;
+const unsigned long scanInterval = 1000; // сканируем каждые 1 секунду
 
 void setup() {
-  Wire.begin();
   Serial.begin(9600);
+  mySerial.begin(9600);
+
+  SPI.begin();
+  mfrc522.PCD_Init();
+
+  Serial.println("Master ready");
 }
 
 void loop() {
-  for (byte addr = 8; addr < 120; addr++) {
-    Wire.requestFrom(addr, 4);
-    int i = 0;
-    while (Wire.available()) {
-      slaveUID[i++] = Wire.read();
-    }
+  unsigned long currentTime = millis();
 
-    if (i == 4 && !(slaveUID[0] == 0 && slaveUID[1] == 0 && slaveUID[2] == 0 && slaveUID[3] == 0)) {
-      Serial.print("[0x");
-      Serial.print(addr, HEX);
-      Serial.print(", ");
-      for (int j = 0; j < 4; j++) {
-        if (slaveUID[j] < 0x10) Serial.print("0");
-        Serial.print(slaveUID[j], HEX);
-        if (j < 3) Serial.print(":");
+  // Периодическое сканирование на главной
+  if (currentTime - lastScanTime >= scanInterval) {
+    lastScanTime = currentTime;
+    if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
+      String uidStr = "";
+      for (byte i = 0; i < mfrc522.uid.size; i++) {
+        if (mfrc522.uid.uidByte[i] < 0x10) uidStr += "0";
+        uidStr += String(mfrc522.uid.uidByte[i], HEX);
+        if (i < mfrc522.uid.size - 1) uidStr += "-";
       }
-      Serial.println("]");
+      uidStr.toUpperCase();
+
+      Serial.print("Master UID: ");
+      Serial.println(uidStr);
+      mfrc522.PICC_HaltA();
     }
-    delay(50);
   }
 
-  delay(200);
+  // Приём данных от дочерних
+  if (mySerial.available()) {
+    String uid = mySerial.readStringUntil('\n');
+    childCounter++;
+    Serial.print("Child ");
+    Serial.print(childCounter);
+    Serial.print(" UID: ");
+    Serial.println(uid);
+  }
 }
--- ReadingRFID/slave_sketch.ino
+++ ReadingRFID/slave_sketch.ino
@@ -1,32 +1,48 @@
-#include <Wire.h>
 #include <SPI.h>
 #include <MFRC522.h>
+#include <SoftwareSerial.h>
 
-#define I2C_ADDRESS 0x10
-#define RST_PIN     9
-#define SS_PIN      10
+#define RST_PIN 9
+#define SS_PIN 10
+
+#define RX_PIN 2
+#define TX_PIN 3
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
-byte uidBuffer[4] = {0};
+SoftwareSerial mySerial(RX_PIN, TX_PIN);
+
+unsigned long lastSendTime = 0;
+const unsigned long sendInterval = 1500;  // минимальный интервал отправки UID (1.5 сек)
 
 void setup() {
-  Wire.begin(I2C_ADDRESS);
-  Wire.onRequest(sendUID);
+  Serial.begin(9600);
+  mySerial.begin(9600);
+
   SPI.begin();
   mfrc522.PCD_Init();
+
+  Serial.println("Slave ready");
 }
 
 void loop() {
-  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
-    for (byte i = 0; i < 4; i++) {
-      uidBuffer[i] = mfrc522.uid.uidByte[i];
-    }
-    mfrc522.PICC_HaltA();
-  }
-}
+  unsigned long currentTime = millis();
 
-void sendUID() {
-  Wire.write(uidBuffer, 4);
-  // Сброс UID после отправки
-  for (byte i = 0; i < 4; i++) uidBuffer[i] = 0;
+  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
+    if (currentTime - lastSendTime > sendInterval) {
+      String uidStr = "";
+      for (byte i = 0; i < mfrc522.uid.size; i++) {
+        if (mfrc522.uid.uidByte[i] < 0x10) uidStr += "0";
+        uidStr += String(mfrc522.uid.uidByte[i], HEX);
+        if (i < mfrc522.uid.size - 1) uidStr += "-";
+      }
+      uidStr.toUpperCase();
+
+      mySerial.println(uidStr);
+      Serial.print("Sent UID: ");
+      Serial.println(uidStr);
+
+      mfrc522.PICC_HaltA();
+      lastSendTime = currentTime;
+    }
+  }
 }
